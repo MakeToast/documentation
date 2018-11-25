02cf66b976fefb8aa452dbf4e964e38e174af157
choeyujin <barbby20@naver.com>
2018-11-25 09:25:45 -0500

    Caculator code

---
+++ documentation/C#/Calculator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+using System.Windows.Form;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public partial class Program : Form
+    {
+        public Calculator()
+        {
+            InitializeComponent();
+        }
+        private void HelloLabel_Click(object sender, EventArgs e)
+        {
+            int number1 = 1;
+            int number2 = 2;
+
+            int sum = number1 + number2;
+            HelloLabel.Text = sum.ToString(); // HelloLabel.Text : string
+
+            
+        }
+    }
+}
