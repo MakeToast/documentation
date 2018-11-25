1dbb38c41ac4c581916886543790631a8636a2b8
choeyujin <barbby20@naver.com>
2018-11-25 09:19:08 -0500

    Example code

---
+++ documentation/C#/Example/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello C#!");
+            Console.WriteLine(args.Length);
+            Console.ReadKey();
+        }
+    }
+}
